cdc2d70b2443aaed4d4a11a93e32ac346ea4a39c
Martin Simanjuntak <61620168+martinsimanjuntak@users.noreply.github.com>
2021-08-24 12:23:28 +0700

    Tugas GenerateInvoice

---
+++ GenerateInvoice/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using RomanNumerals;
+namespace GenerateInvoice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int counter = 10982;
+           Console.WriteLine(GenerateInvoice(counter)); 
+        }
+
+        private static string GenerateInvoice(int count)
+        {            
+            string temp1 = DateTime.Now.ToString("yyyyMM");
+            string temp2 = DateTime.Now.ToString("ddd").Substring(0, 2).ToUpper();
+            int day = DateTime.Now.Day;
+            int year = DateTime.Now.Year;
+            var roman_day = new RomanNumeral(day);
+            var roman_year = new RomanNumeral(year).ToString().Substring(2);
+
+            return $"INV/{temp1}/{temp2}/{roman_day}/{roman_year}/{++count}";
+        }
+      
+    }
+}
